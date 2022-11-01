a41b5cda7a88daf3e6cfffa3f76eac302b1b76b6
Cruz Maldonado <cruzmaldonadocharris@gmail.com>
2022-11-01 19:24:05 -0400

    creando ruta,servio y controlador de recipes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 //? Dependencies
 const express = require('express');
 const db = require('./utils/database')
+const Cors = require ("cors")
 
 //? Files
 const {port} = require('./config');
 //* Routes
 const userRouter = require('./users/users.router')
 const authRouter = require('./auth/auth.router')
+const categoryRouter = require('./categories/categories.router')
+const recipeRouter = require('./recipes/recipes.router')
 const initModels = require('./models/initModels')
+
 
 //? Initial Configs
 const app = express()
 
 app.use(express.json())
+app.use(Cors())
 
 db.authenticate()
     .then(() => {
@@ -42,6 +47,8 @@ app.get('/',(req, res) => {
 
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/categories', categoryRouter)
+app.use('/api/v1/recipes', recipeRouter)
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`)
